238ab8eccf9f358cb4d9178562669f981441bdaf
shahriar newaz <shahriar.newaz@sonymobile.com>
2014-12-16 15:10:21 +0100

    test4

--- modules/client/client.ctrl.js
+++ modules/client/client.ctrl.js
@@ -2,8 +2,29 @@
 'use strict';
 (function () {
     var clientModule = angular.module('probeApp.client');
-    clientModule.controller('ClientCtrl', ['$scope',function ($scope) {
+    clientModule.controller('ClientCtrl', ['$scope','$http',function ($scope, $http) {
 
+        $http.get('/api/ProbeInstallations').success(function (data) {
+            $scope.devices = data;
+            $scope.orderProp = 'appId';
+        });
+
+        $scope.notify = function (id, msg) {
+            $http.post('/notify/' + encodeURIComponent(id), {msg: msg}).success(function (data, status, headers) {
+                $scope.status = 'Notification sent: ' + data + ' status: ' + status;
+            });
+        };
+
+        $scope.delete = function (index, id) {
+            $http.delete('/api/ProbeInstallations/' + encodeURIComponent(id)).success(function (data, status, headers) {
+                $scope.devices.splice(index, 1);
+                $scope.status = 'Record deleted: ' + id + ' status: ' + status;
+            });
+        };
+
+        $scope.getAction = function (status) {
+            return 'Active' === status ? 'Deactivate' : 'Activate';
+        };
 
     }]);
 
